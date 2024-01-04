e1eeed47a3fef2e844d751ad5324512d50ecbb31
Leonardo Grochoski <79224815+LeoGrochoski@users.noreply.github.com>
2024-01-03 23:59:59 -0300

    Update Crud com Python e MySQL.py
    editando arquivo de codigo

--- Crud com Python e MySQL.py
+++ Crud com Python e MySQL.py
@@ -1,49 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # CRUD com Python e MySQL
-
-# <img src="https://irias.com.br/blog/wp-content/uploads/2021/02/python-mysql.png" alt="Python e MySQL">
-# 
-# 
-# ## O que é CRUD?
-# 
-# CRUD é um acrônimo que representa quatro operações básicas realizadas em sistemas de gerenciamento de banco de dados relacionais:
-# 
-# 1. **Create (Criar)**: Permite a criação de novos registros ou entradas no banco de dados. Em termos simples, é a operação de adicionar informações a um banco de dados.
-# 
-# 2. **Read (Ler)**: Permite a leitura ou recuperação de informações existentes do banco de dados. Essa operação é usada para visualizar ou obter dados armazenados.
-# 
-# 3. **Update (Atualizar)**: Permite a modificação de registros ou informações existentes no banco de dados. É a operação usada para fazer alterações em dados já armazenados.
-# 
-# 4. **Delete (Excluir)**: Permite a remoção de registros ou informações do banco de dados. Essa operação é usada para eliminar dados que não são mais necessários.
-# 
-# ## Como aplicar?
-# 
-# O CRUD é uma abordagem fundamental no desenvolvimento de sistemas que envolvem interação com bancos de dados. Ele fornece um conjunto padrão de operações para manipulação de dados, garantindo a integridade e consistência das informações no banco de dados.
-# 
-# Em resumo, o CRUD é utilizado para criar, ler, atualizar e excluir dados em um sistema, representando as operações essenciais para gerenciar informações em um banco de dados. 
-# 
-# 
-# 
-
-# ## Instalação e preparação
-
-# Primeiramete temos que instalar o conector do mysql. Este comando utiliza a linha de comando pip install para instalar o módulo mysql-connector-python. O "!" é utilizado para executar comandos do sistema diretamente no ambiente Jupyter Notebook.
-
-# In[ ]:
+# CRUD com Python e MySQL
 
 
-get_ipython().system('pip install mysql-connector-python')
-
-
-# In[16]:
+pip install mysql-connector-python
 
 
 import mysql.connector
-
-
-# In[17]:
 
 
 conexao = mysql.connector.connect(
@@ -55,24 +16,7 @@ conexao = mysql.connector.connect(
 
 cursor = conexao.cursor()
 
-
-# O código acima importa o módulo mysql.connector que fornece funcionalidades para interagir com bancos de dados MySQL usando Python.
-# 
-# Em seguida, ele estabelece uma conexão com um banco de dados MySQL local. Os parâmetros fornecidos são:
-#     
-# host: o endereço do servidor do banco de dados (neste caso, 'localhost' indica que o banco de dados está na mesma máquina).
-# user: o nome de usuário usado para autenticar no banco de dados.
-# password: a senha associada ao usuário.
-# database: o nome do banco de dados ao qual se deseja conectar.
-#     
-# O código cria um objeto cursor, que é utilizado para executar comandos SQL no banco de dados.
-
-# ### Criando informações no Banco
-
-# In[18]:
-
-
-# CREATE
+# 1. Criando informações no Banco
 
 NOME_PRODUTO = "chocolate"
 VALOR = 15
@@ -81,13 +25,45 @@ comando = f'INSERT INTO produtos (NOME_PRODUTO, VALOR) VALUES ("{NOME_PRODUTO}",
 
 cursor.execute(comando)
 
-conexao.commit() # edita o banco de dados
+conexao.commit()
 
+NOME_PRODUTO = "MOLHO DE TOMATE"
+VALOR = 8
 
-# ### Lendo informações do banco de dados
+comando = f'INSERT INTO produtos (NOME_PRODUTO, VALOR) VALUES ("{NOME_PRODUTO}", {VALOR})'
 
-# In[ ]:
+cursor.execute(comando)
 
+conexao.commit() 
 
+# 2. Lendo informações do banco de dados
+
+comando = f'SELECT * FROM produtos'
+
+cursor.execute(comando)
+resultado = cursor.fetchall() # ler o banco de dados
+
+print(resultado)
+
+# 3. Atualizando informações no banco de dados
+
+NOME_PRODUTO = "AGUA C/ GÁS"
+VALOR = 4
+
+comando = f'UPDATE produtos SET VALOR = {valor} WHERE NOME_PRODUTO = "{NOME_PRODUTO}"'
+
+cursor.execute(comando)
+
+conexao.commit()
+
+# 4. Deletando informações do banco de dados
+
+NOME_PRODUTO = "CHOCOLATE"
+
+comando = f'DELETE FROM produtos WHERE nome_produto = "{NOME_PRODUTO}"'
+
+cursor.execute(comando)
+
+conexao.commit()
 
 
